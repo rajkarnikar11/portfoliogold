120829ebe4bf9e1680c547c878fa1e46020d030a
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-04-10 10:46:31 +0545

    card hover update

--- src/Components/CardSection.tsx
+++ src/Components/CardSection.tsx
@@ -3,19 +3,35 @@ import Arrow from "../assets/arrow.svg"
 import '../Styles/Card.css' ;
 const CardSection = (props: any) => {
   return (
-    <a className='card-continer' href={props.link}>
-      <div className='hover-element'>
-        <p>visit link</p>  
-        <div>
-          <img height={100} src={Arrow} alt=">"/> 
+    <a target={"_blank"} className='card-continer' href={props.link}>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='over'/>
+      <div className='tilt-box'> 
+          <div className='bar' />
+          <div className='card-title'>{props.name}</div>
+          {/* <div className='card-title-2'>{props.name}</div>
+          <div className='card-title-3'>{props.name}</div>
+          <div className='card-title-4'>{props.name}</div>
+          <div className='card-title-5'>{props.name}</div>
+          <div className='card-title-6'>{props.name}</div>
+          <div className='card-title-7'>{props.name}</div>
+          <div className='card-title-8'>{props.name}</div>
+          <div className='card-title-9'>{props.name}</div>
+          <div className='card-title-10'>{props.name}</div> */}
+          <div className='bar-2' />
+          
         </div>
-      </div>
-      <div className='card-title'>{props.name}</div>
-      <div className='card-image-container'>
-        <img className='card-image' src={props.image} alt="proimg"/>
-       
-              
-      </div>
+     
     </a>
     )
 }
